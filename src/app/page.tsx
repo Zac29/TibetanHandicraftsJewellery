e0f4a06cad5a7849066352c686fe04b5807898a1
Aayush766 <blaze.aayush23@gmail.com>
2026-01-20 21:43:36 +0530

    Testinomial Section Implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Catogery from "../components/home/catogery";
-import Hero from "../components/home/Hero";
+import Catogery from "../components/layout/home/catogery";
+import Hero from "../components/layout/home/Hero";
 import ProductsGrid from "../components/products/ProductsGrid";
 import ScaleWrapper from "../components/layout/ScaleWrapper";
-import ProductShowcase from "../components/home/ProductShowcase";
+import ProductShowcase from "../components/layout/home/ProductShowcase";
 import Footer from "../components/layout/Footer";
+import Testimonials from "../components/layout/home/Testimonials";
 export default function Home() {
   return (
     <ScaleWrapper>
@@ -11,6 +12,7 @@ export default function Home() {
       <Catogery />
       <ProductsGrid />
       <ProductShowcase />
+      <Testimonials />
       <Footer />
     </ScaleWrapper>
   );
